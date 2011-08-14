ca938090107dcbf26fbc88fed77d530a6691694e
Ron Dollete <ron@cobaltroad.com>
2011-08-14 13:16:57 -0700

    Updating team JSON to include points

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,8 @@ class Team < ActiveRecord::Base
     players.inject(0) { |sum, player| sum += player.points }
   end
 
+  def as_json(options={})
+    super(options.merge(:methods => [:points]))
+  end
+
 end
